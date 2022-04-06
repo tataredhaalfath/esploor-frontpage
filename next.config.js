783a3e9f77eb7d8ecfbffb3cb1e6d70c588f3e0c
Tata Redha Al Fath <tataredhaalfath13@gmail.com>
2022-04-06 14:05:09 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withCss = require('@zeit/next-css');
+const withReactSvg = require('next-react-svg');
+const withImages = require('next-images');
+const path = require('path');
+
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+
+module.exports = withPlugins([
+  withCss({}),
+  withImages({}),
+  withReactSvg({
+    include: path.resolve(__dirname,"./public/images"),
+    webpack(config, options) {
+      return config;
+    }
+  }),
+])
---
+++ pages/hello.js
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function hello() {
+  return <div>hello</div>;
+}
